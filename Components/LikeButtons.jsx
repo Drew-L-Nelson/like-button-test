0b3412e9cb3ed0664655f14a92037bc857527e2d
Drew Nelson <90492724+Drew-L-Nelson@users.noreply.github.com>
2024-03-06 23:03:05 +0000

    card added, Upvotes, Downvotes, Stars added into card

--- Components/LikeButtons.jsx
+++ Components/LikeButtons.jsx
@@ -1,39 +1,69 @@
 import * as React from "react";
 import { ThemeProvider, createTheme } from '@mui/system';
+import Card from '@mui/material/Card';
+import Stack from '@mui/material/Stack';
+import Divider from '@mui/material/Divider';
+import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Fab from '@mui/material/Fab';
 import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import NavigationIcon from "@mui/icons-material/Navigation";
+import StarIcon from '@mui/icons-material/Star';
 import { styled } from "@mui/material/styles";
 
 export default function LikeButtons() {
   return (
-    <StyledBox>
-        <Box>
+    <Card variant="outlined" sx={{ maxWidth: 360, backgroundColor: '#f5f5f5', borderColor: '#e0e0e0', borderWidth: 1, borderRadius: '10px', }}>
+        <Box sx={{ p: 2 }}>
             
+            <Stack direction="row" justifyContent="space-between" alignItems="center">
+                <Typography gutterBottom variant="h5" component="div">
+                    Upvotes
+                </Typography>
+                <Typography gutterBottom variant="h6" component="div">
+                    $4.50
+                </Typography>
+            </Stack>
+            
+            <Stack direction="row" justifyContent="space-between" alignItems="center">
+                <Typography gutterBottom variant="h5" component="div">
+                    Downvotes
+                </Typography>
+                <Typography gutterBottom variant="h6" component="div">
+                    $4.50
+                </Typography>
+
+            </Stack>
+
+            <Stack direction="row" justifyContent="space-between" alignItems="center">
+                <Typography gutterBottom variant="h5" component="div">
+                    Stars
+                </Typography>
+                <Typography gutterBottom variant="h6" component="div">
+                    $4.50
+                </Typography>
+
+            </Stack>
+            <Divider sx={{ my: 2 }} />
         </Box>
-        <Box sx={{ "& > :not(style)": { ml: 5 } }}>
+        
+        <Box sx={{ "& > :not(style)": { ml: 5 }, p: 3, mt: -4 }}>
             <Fab color="primary" aria-label="add">
                 <ArrowUpwardIcon />
             </Fab>
             <Fab color="secondary" aria-label="subtract">
                 <ArrowDownwardIcon />
             </Fab>
-            <Fab variant="extended">
-                <NavigationIcon sx={{ mr: 1 }} />
-                Navigate
-            </Fab>
             <Fab aria-label="like">
-                <FavoriteIcon />
+                <StarIcon />
             </Fab>
         </Box>
-    </StyledBox>
+    </Card>
   );
 }
 
 const StyledBox = styled(Box)({
-    display: "flex",
-    flexDirection: "column"
+    // display: "flex",
+    // flexDirection: "column",
+    justifyContent: "center",
 });
